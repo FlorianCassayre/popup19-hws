664c89bf9bb594196c1ac81027ad59d3265964e6
Florian Cassayre <florian.cassayre@gmail.com>
2019-02-01 20:20:10 +0100

    Solved Turbo using Fenwick trees.

--- src/main/scala/popup/hw2/Turbo.java
+++ src/main/scala/popup/hw2/Turbo.java
@@ -1,6 +1,5 @@
 package popup.hw2;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Turbo {
@@ -8,28 +7,80 @@ public class Turbo {
         final Scanner scanner = new Scanner(System.in);
 
         final int n = scanner.nextInt();
-        final ArrayList<Integer> list = new ArrayList<>(n);
+        final int[] indexes = new int[n];
+        final BinaryIndexedTree left = new BinaryIndexedTree(n), right = new BinaryIndexedTree(n);
         for(int i = 0; i < n; i++) {
             final int v = scanner.nextInt() - 1;
-            list.add(v);
+            indexes[v] = i;
+            left.add(i, 1);
+            right.add(i, 1);
         }
 
         for(int i = 0; i < n; i++) {
-            final int k, index, swaps;
+            final int index;
+            final long swaps;
             if(i % 2 == 0) { // Low
-                k = i / 2;
-                index = list.indexOf(k);
-                swaps = index;
+                index = indexes[i / 2];
+                swaps = left.sum(index);
+                left.add(index, -1);
+                right.add(n - 1 - index, -1);
             } else { // High
-                k = n - 1 - i / 2;
-                index = list.indexOf(k);
-                swaps = list.size() - 1 - index;
+                index = n - 1 - indexes[n - 1 - i / 2];
+                swaps = right.sum(index);
+                right.add(index, -1);
+                left.add(n - 1 - index, -1);
             }
-            list.remove(index);
-
             System.out.println(swaps);
         }
 
         scanner.close();
+    }
+
+    private static final class BinaryIndexedTree {
+
+        private final long[] array;
+
+        /**
+         * Creates a new Fenwick tree (indexed binary tree) data structure.
+         * @param n the size of the array
+         */
+        public BinaryIndexedTree(int n) {
+            this.array = new long[n];
+        }
+
+        /**
+         * Adds delta to a[i].
+         * @param i the index in the array
+         * @param delta the number to add
+         */
+        public void add(int i, long delta) {
+            if(i < 0 || i >= array.length)
+                throw new IndexOutOfBoundsException(String.valueOf(i));
+
+            i++;
+            while(i <= array.length) {
+                array[i - 1] += delta;
+                i += Integer.lowestOneBit(i);
+            }
+        }
+
+        /**
+         * Computes the prefix sum a[0] + a[1] + ... + a[end - 1].
+         * If the index is zero, the result is zero.
+         * @param end the end index (exclusive)
+         * @return the computed prefix sum
+         */
+        public long sum(int end) {
+            if(end < 0 || end > array.length)
+                throw new IndexOutOfBoundsException(String.valueOf(end));
+
+            long sum = 0;
+            while(end > 0) {
+                sum += array[end - 1];
+                end -= Integer.lowestOneBit(end);
+            }
+
+            return sum;
+        }
     }
 }

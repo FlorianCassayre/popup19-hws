012a8114ec8cbf580a0f26754c7f6ab2064c76ea
Florian Cassayre <florian.cassayre@gmail.com>
2019-02-05 16:21:27 +0100

    Improved Turbo.

--- src/main/scala/popup/hw2/Turbo.java
+++ src/main/scala/popup/hw2/Turbo.java
@@ -8,28 +8,20 @@ public class Turbo {
 
         final int n = scanner.nextInt();
         final int[] indexes = new int[n];
-        final BinaryIndexedTree left = new BinaryIndexedTree(n), right = new BinaryIndexedTree(n);
+        final BinaryIndexedTree tree = new BinaryIndexedTree(n);
         for(int i = 0; i < n; i++) {
             final int v = scanner.nextInt() - 1;
             indexes[v] = i;
-            left.add(i, 1);
-            right.add(i, 1);
+            tree.add(i, 1);
         }
 
         for(int i = 0; i < n; i++) {
-            final int index;
-            final long swaps;
-            if(i % 2 == 0) { // Low
-                index = indexes[i / 2];
-                swaps = left.sum(index);
-                left.add(index, -1);
-                right.add(n - 1 - index, -1);
-            } else { // High
-                index = n - 1 - indexes[n - 1 - i / 2];
-                swaps = right.sum(index);
-                right.add(index, -1);
-                left.add(n - 1 - index, -1);
-            }
+            final boolean low = i % 2 == 0;
+            final int index = indexes[low ? i / 2 : n - 1 - i / 2];
+            tree.add(index, -1);
+
+            final long sum = tree.sum(index), swaps = low ? sum : n - i - sum - 1;;
+
             System.out.println(swaps);
         }
 

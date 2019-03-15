f8f5ae970e8739f2942bff3e5dda422b5cf84945
Florian Cassayre <florian.cassayre@gmail.com>
2019-03-15 20:46:01 +0100

    Gopher now passing with maximum bipartite matching.

--- src/main/scala/popup/hw6/Gopher.java
+++ src/main/scala/popup/hw6/Gopher.java
@@ -8,6 +8,7 @@ public class Gopher {
         final Scanner scanner = new Scanner(System.in);
 
         // => Max-flow problem!
+        // ===> Maximum bipartite matching
 
         while(scanner.hasNextInt()) {
 
@@ -15,31 +16,55 @@ public class Gopher {
             final int n = Integer.parseInt(ints[0]), m = Integer.parseInt(ints[1]), s = Integer.parseInt(ints[2]), v = Integer.parseInt(ints[3]);
             final int distanceLimitSq = sq(s * v * SCALE);
 
-            final List<Point> sources = new ArrayList<>(n), sinks = new ArrayList<>(m);
+            final Point[] gophers = new Point[n], holes = new Point[m];
             for(int i = 0; i < n; i++)
-                sources.add(readPoint(scanner));
+                gophers[i] = readPoint(scanner);
             for(int i = 0; i < m; i++)
-                sinks.add(readPoint(scanner));
+                holes[i] = readPoint(scanner);
 
-            final List<FlowSolver.Edge> edges = new ArrayList<>();
-            final int source = n + m, sink = source + 1;
-            for(int i = 0; i < n; i++) // Link the source to all the gophers
-                edges.add(new FlowSolver.Edge(source, i, 1));
+            final boolean[][] graph = new boolean[n][m];
             for(int i = 0; i < n; i++) // Link all the gopher to the holes they can reach
                 for(int j = 0; j < m; j++)
-                    if(sources.get(i).distanceSq(sinks.get(j)) <= distanceLimitSq)
-                        edges.add(new FlowSolver.Edge(i, n + j, 1));
-            for(int i = 0; i < m; i++) // Link all the the holes to the sink
-                edges.add(new FlowSolver.Edge(n + i, sink, 1));
+                    if(gophers[i].distanceSq(holes[j]) <= distanceLimitSq)
+                        graph[i][j] = true;
 
-            final FlowSolver.Solution solution = new FlowSolver(n + m + 2, edges, source, sink).maximumFlow();
+            final int maxMatching = maximumBipartiteMatching(n, m, graph);
 
-            final long vulnerable = n - solution.f;
+            final long vulnerable = n - maxMatching;
 
             System.out.println(vulnerable);
         }
 
         scanner.close();
+    }
+
+    private static int maximumBipartiteMatching(int n, int m, boolean[][] graph) {
+        int[] assigned = new int[n];
+        for(int i = 0; i < n; i++)
+            assigned[i] = -1;
+        int paths = 0;
+
+        for(int u = 0; u < m; u++) {
+            boolean[] visited = new boolean[n];
+            for(int i = 0; i < n; i++)
+                visited[i] = false;
+            if(bipartiteMatch(u, visited, assigned, graph))
+                paths++;
+        }
+        return paths;
+    }
+
+    private static boolean bipartiteMatch(int u, boolean[] visited, int[] assigned, boolean[][] graph) {
+        for(int v = 0; v < graph.length; v++) {
+            if(graph[u][v] && !visited[v]) {
+                visited[v] = true;
+                if(assigned[v] < 0 || bipartiteMatch(assigned[v], visited, assigned, graph)) {
+                    assigned[v] = u;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private static Point readPoint(Scanner scanner) {
@@ -61,129 +86,6 @@ public class Gopher {
 
         public int distanceSq(Point that) {
             return sq(this.x - that.x) + sq(this.y - that.y);
-        }
-    }
-
-    private static class FlowSolver {
-
-        private final int n;
-        private final int s, t;
-        private final long[][] capacities, flows;
-        private final long[] height, excess;
-        private final int[] seen;
-
-        public FlowSolver(int n, List<Edge> edges, int s, int t) {
-            this.n = n;
-            this.s = s;
-            this.t = t;
-            this.capacities = new long[n][n];
-            this.flows = new long[n][n];
-            this.height = new long[n];
-            this.excess = new long[n];
-            this.seen = new int[n];
-
-            for(Edge edge : edges) // Initialize capacity matrix
-                capacities[edge.u][edge.v] += edge.capacity;
-        }
-
-        public Solution maximumFlow() {
-            // Great inspiration of: https://en.wikipedia.org/wiki/Push%E2%80%93relabel_maximum_flow_algorithm
-
-            final LinkedList<Integer> queue = new LinkedList<>();
-            for(int i = 0; i < n; i++)
-                if(i != s && i != t)
-                    queue.add(i);
-
-            height[s] = n;
-            excess[s] = Long.MAX_VALUE;
-            for(int v = 0; v < n; v++)
-                push(s, v);
-
-            Iterator<Integer> it = queue.iterator();
-            while(it.hasNext()) {
-                int u = it.next();
-                long oldHeight = height[u];
-                discharge(u);
-                if(height[u] > oldHeight) {
-                    it.remove();
-                    queue.addFirst(u); // Relabel-to-front selection rule
-                    it = queue.iterator();
-                }
-            }
-
-            return new Solution();
-        }
-
-        private void push(int u, int v) {
-            final long send = Math.min(excess[u], capacities[u][v] - flows[u][v]);
-            flows[u][v] += send;
-            flows[v][u] -= send;
-            excess[u] -= send;
-            excess[v] += send;
-        }
-
-        private void relabel(int u) {
-            long min = Long.MAX_VALUE;
-            for(int v = 0; v < n; v++)
-                if(capacities[u][v] - flows[u][v] > 0) {
-                    min = Math.min(min, height[v]);
-                    if(min < Long.MAX_VALUE)
-                        height[u] = min + 1;
-                }
-        }
-
-        private void discharge(int u) {
-            while(excess[u] > 0) {
-                if(seen[u] < n) {
-                    int v = seen[u];
-                    if(capacities[u][v] - flows[u][v] > 0 && height[u] > height[v])
-                        push(u, v);
-                    else
-                        seen[u]++;
-                } else {
-                    relabel(u);
-                    seen[u] = 0;
-                }
-            }
-        }
-
-        public final class Solution {
-            public final long f;
-            public final List<Edge> flow;
-
-            private Solution() {
-                long f = 0; // Max-flow
-                for(int i = 0; i < n; i++)
-                    f += flows[s][i];
-                this.f = f;
-                this.flow = new ArrayList<>();
-                for(int u = 0; u < n; u++)
-                    for(int v = 0; v < n; v++)
-                        if(flows[u][v] > 0)
-                            this.flow.add(new Edge(u, v, flows[u][v]));
-                Collections.sort(flow);
-            }
-        }
-
-        public static final class Edge implements Comparable<Edge> {
-            public final int u, v;
-            public final long capacity;
-
-            public Edge(int u, int v, long capacity) {
-                this.u = u;
-                this.v = v;
-                this.capacity = capacity;
-            }
-
-            @Override
-            public int compareTo(Edge that) {
-                if(this.u != that.u)
-                    return Integer.compare(this.u, that.u);
-                else if(this.v != that.v)
-                    return Integer.compare(this.v, that.v);
-                else
-                    return Long.compare(this.capacity, that.capacity);
-            }
         }
     }
 }

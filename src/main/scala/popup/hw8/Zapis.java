a07d66e24806d4412afad280cf195686b5760cce
Florian Cassayre <florian.cassayre@gmail.com>
2019-04-03 16:02:19 +0200

    Bogus zapis.

---
+++ src/main/scala/popup/hw8/Zapis.java
@@ -0,0 +1,63 @@
+package popup.hw8;
+
+import java.util.*;
+
+public class Zapis {
+    private static final long MOD = 100_000;
+    private static final String OPENING = "([{", CLOSING = ")]}";
+    private static final Map<Character, Character> OPPOSITE = Collections.unmodifiableMap(new HashMap<Character, Character>() {{
+        for(int i = 0; i < OPENING.length(); i++)
+            put(OPENING.charAt(i), CLOSING.charAt(i));
+    }});
+    private static final char JOKER = '?';
+
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        final int n = Integer.parseInt(scanner.nextLine());
+        final String string = scanner.nextLine();
+
+        final Long[][] cache = new Long[n][n + 1];
+
+        final long result = recurrence(0, n, string, cache);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+
+    private static long recurrence(int from, int until, String string, Long[][] cache) {
+
+        if(until < from)
+            return 0;
+        if(from == until)
+            return 1;
+
+        if(cache[from][until - 1] != null)
+            return cache[from][until - 1];
+
+        final long value;
+
+        if(CLOSING.indexOf(string.charAt(from)) == -1) {
+            final Character opposite = OPPOSITE.get(string.charAt(from));
+
+            long sum = 0;
+
+            for(int i = from + 1; i < until; i++) {
+                if(string.charAt(i) == JOKER || (CLOSING.indexOf(string.charAt(i)) != -1 && (string.charAt(from) == JOKER || string.charAt(i) == opposite))) {
+                    final int p = string.charAt(from) == JOKER && string.charAt(i) == JOKER ? 3 : 1;
+                    sum = (sum + p * recurrence(from + 1, i, string, cache) * recurrence(i + 1, until, string, cache)) % MOD;
+                }
+            }
+
+            value = sum;
+        } else { // Unbalanced
+            value = 0;
+        }
+
+        cache[from][until - 1] = value;
+
+        return value;
+    }
+}
